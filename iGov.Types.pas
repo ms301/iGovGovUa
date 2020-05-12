e8640abd1544067a98a897718398cd99a6ee50d8
MAXIM-SYSOEV\WorkPC <work@maks4a.gmail>
2020-05-12 09:59:47 +0300

    Переименование типов

--- iGov.Types.pas
+++ iGov.Types.pas
@@ -6,46 +6,88 @@ uses
   System.JSON.Serializers;
 
 type
-  TIgoCity = class
+  TigovService = class
+  private
+    [JsonName('aServiceData')]
+    FServiceData: string;
+    [JsonName('nID')]
+    FID: Integer;
+    [JsonName('nID_Status')]
+    FID_Status: Integer;
+    [JsonName('nOpenedLimit')]
+    FOpenedLimit: Integer;
+    [JsonName('nOrder')]
+    FOrder: Integer;
+    [JsonName('nStatus')]
+    FStatus: Integer;
+    [JsonName('nSub')]
+    FSub: Integer;
+    [JsonName('sFAQ')]
+    FFAQ: string;
+    [JsonName('sInfo')]
+    FInfo: string;
+    [JsonName('sLaw')]
+    FLaw: string;
+    [JsonName('sName')]
+    FName: string;
+    [JsonName('sSubjectOperatorName')]
+    FSubjectOperatorName: string;
+    [JsonName('saKeyword')]
+    FKeyword: string;
+  public
+    property ServiceData: string read FServiceData write FServiceData;
+    property ID: Integer read FID write FID;
+    property IDStatus: Integer read FID_Status write FID_Status;
+    property OpenedLimit: Integer read FOpenedLimit write FOpenedLimit;
+    property Order: Integer read FOrder write FOrder;
+    property Status: Integer read FStatus write FStatus;
+    property Sub: Integer read FSub write FSub;
+    property FAQ: string read FFAQ write FFAQ;
+    property Info: string read FInfo write FInfo;
+    property Law: string read FLaw write FLaw;
+    property Name: string read FName write FName;
+    property SubjectOperatorName: string read FSubjectOperatorName write FSubjectOperatorName;
+    property Keyword: string read FKeyword write FKeyword;
+  end;
+
+  TigoCatalogTree = class
+  private
+    [JsonName('aService')]
+    FService: TArray<TigovService>;
+  public
+    property Service: TArray<TigovService> read FService write FService;
+
+  end;
+
+  TigovCity = class
   private
     [JsonName('nID')]
-    FID: integer;
+    FID: Integer;
     [JsonName('sID_UA')]
     FIDText: string;
     [JsonName('sName')]
     FName: string;
   public
-    property ID: integer read FID write FID;
+    property ID: Integer read FID write FID;
     property IDText: string read FIDText write FIDText;
     property Name: string read FName write FName;
   end;
 
-  TigoRegions = class
+  TigovRegions = class
   private
     [JsonName('nID')]
-    FID: integer;
+    FID: Integer;
     [JsonName('sID_UA')]
     FIDText: string;
     [JsonName('sName')]
     FName: string;
     [JsonName('aCity')]
-    FCity: TArray<TIgoCity>;
+    FCity: TArray<TigovCity>;
   public
-    property ID: integer read FID write FID;
+    property ID: Integer read FID write FID;
     property IDText: string read FIDText write FIDText;
     property Name: string read FName write FName;
-    property City: TArray<TIgoCity> read FCity write FCity;
-  end;
-
-  TService = class
-  private
-    FOpenedLimit: integer;
-    FSubjectOperatorName: string;
-    FKeyword: string;
-  public
-    property OpenedLimit: integer read FOpenedLimit write FOpenedLimit;
-    property SubjectOperatorName: string read FSubjectOperatorName write FSubjectOperatorName;
-    property Keyword: string read FKeyword write FKeyword;
+    property City: TArray<TigovCity> read FCity write FCity;
   end;
 
   TSubCategory = class
@@ -61,17 +103,17 @@ type
     [JsonName('aSubcategory')]
     FSubcategory: TArray<TSubCategory>;
     [JsonName('nID')]
-    FID: integer;
+    FID: Integer;
     [JsonName('nOrder')]
-    FOrder: integer;
+    FOrder: Integer;
     [JsonName('sID')]
     FIDText: string;
     [JsonName('sName')]
     FName: string;
   public
     property Subcategory: TArray<TSubCategory> read FSubcategory write FSubcategory;
-    property ID: integer read FID write FID;
-    property Order: integer read FOrder write FOrder;
+    property ID: Integer read FID write FID;
+    property Order: Integer read FOrder write FOrder;
     property IDText: string read FIDText write FIDText;
     property Name: string read FName write FName;
   end;
