67a42be58bf3f38976f3d1c25757da3556f5e504
MAXIM-SYSOEV\WorkPC <work@maks4a.gmail>
2020-05-14 15:27:19 +0300

    Начало разработки клиента

---
+++ Sample/Client/Unit9.pas
@@ -0,0 +1,29 @@
+unit Unit9;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts,
+  FMX.TreeView, FMX.TabControl;
+
+type
+  TForm9 = class(TForm)
+    TabControl1: TTabControl;
+    TabItem1: TTabItem;
+    TabItem2: TTabItem;
+    TreeView1: TTreeView;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form9: TForm9;
+
+implementation
+
+{$R *.fmx}
+
+end.
